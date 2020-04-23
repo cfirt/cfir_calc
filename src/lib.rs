024f83e9571e60c57aa453279da076eac0babb47
Cfir Tsabari <cfirt@sentinelone.com>
2020-04-23 15:13:49 +0300

    FirstCommit

---
+++ src/lib.rs
@@ -0,0 +1,14 @@
+
+pub fn sumu32(a: u32,b:u32) -> u32{
+    return a+b
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+    #[test]
+
+    fn it_works() {
+        assert_eq!(sumu32(2,2), 4);
+    }
+}
